765983093a645a5a26ac178e3844bcad86d56bc9
Brett Gottula <bgottula@gmail.com>
2017-08-07 22:55:05 -0700

    #8: Adds arguments to set backlash compensation to blind_track.py

--- blind_track.py
+++ blind_track.py
@@ -6,7 +6,7 @@ import error
 import ephem
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('tle', help='filename of two-line element (TLE) target ephemeris')
 parser.add_argument('--scope', help='serial device for connection to telescope', default='/dev/ttyUSB0')
 parser.add_argument('--lat', required=True, help='latitude of observer (+N)')
@@ -15,10 +15,16 @@ parser.add_argument('--elevation', required=True, help='elevation of observer (m
 parser.add_argument('--loop-bw', help='control loop bandwidth (Hz)', default=0.5, type=float)
 parser.add_argument('--loop-damping', help='control loop damping factor', default=0.5, type=float)
 parser.add_argument('--loop-period', help='control loop period', default=0.25, type=float)
+parser.add_argument('--backlash-az', help='backlash in azimuth (deg)', default=0.0, type=float)
+parser.add_argument('--backlash-alt', help='backlash in altitude (deg)', default=0.0, type=float)
+parser.add_argument('--align-dir-az', help='azimuth alignment approach direction (-1 or +1)', default=+1, type=int)
+parser.add_argument('--align-dir-alt', help='altitude alignment approach direction (-1 or +1)', default=+1, type=int)
 args = parser.parse_args()
 
 # Create object with base type TelescopeMount
 mount = mounts.NexStarMount(args.scope)
+mount.set_backlash('az', args.align_dir_az, args.backlash_az)
+mount.set_backlash('alt', args.align_dir_alt, args.backlash_alt)
 
 # Create a PyEphem Observer object
 observer = ephem.Observer()
