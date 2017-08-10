71ef86746cc313f2f6b306a25a086016cfb8f39a
Brett Gottula <bgottula@gmail.com>
2017-08-09 22:09:02 -0700

    #7: Major rewrite of backlash estimation algorithm

--- backlash_estimate.py
+++ backlash_estimate.py
@@ -30,19 +30,35 @@ tracker = track.TrackUntilConverged(
     damping_factor = args.loop_damping
 )
 
+error_lists = {'az': [], 'alt': []}
+
+def tracker_callback():
+    error_lists['az'].append(tracker.error['az'])
+    error_lists['alt'].append(tracker.error['alt'])
+
+def stop_at_half_frame_callback():
+    if (abs(tracker.error['az']) > HALF_FRAME_ERROR_MAG or 
+        abs(tracker.error['alt']) > HALF_FRAME_ERROR_MAG):
+            tracker.stop = True
+
 try:
 
-    MOVEMENT_THRESHOLD_DEG = 20.0 / 3600.0
-    SLEW_STOP_SLEEP = 1.0
-    FAST_SLEW_RATE = 60.0 / 3600.0
-    SLOW_SLEW_RATE = 30.0 / 3600.0
-    MIN_INCREMENT = 1.0 / 3600.0
+    SLEW_STOP_SLEEP = 3.0
+    SLOW_SLEW_RATE = 15.0 / 3600.0
     NUM_ITERATIONS = 5
+    # If the magnitude of the error is larger than this value, the object
+    # is more than 50% of the distance from the center of the frame to the
+    # nearest edge.
+    HALF_FRAME_ERROR_MAG = 0.5 * 0.5 * error_source.degrees_per_pixel * min(
+        error_source.frame_height_px, 
+        error_source.frame_width_px
+    )
 
     backlash_estimates = {}
     for axis in ['az', 'alt']:
 
         backlash_estimates[axis] = []
+        other_axis = 'az' if axis == 'alt' else 'alt'
 
         for i in range(NUM_ITERATIONS):
 
@@ -52,43 +68,105 @@ try:
             # center the object in the FOV
             tracker.run()
 
-            init_error = error_source.compute_error()
-            
-            # Mount is still slewing until commanded to do otherwise.
-            # Intentionally allowing the axis not under test to continue to slew
-            # at the tracking rate so the object is less likely to drift out 
-            # of the FOV on that axis during the test.
+            # Continue tracking for a bit to estimate variance of object's
+            # apparent position due to mount jitter, seeing, tracking loop 
+            # noise, and other effects. Also estimate the slew rate of the
+            # mount.
+            print('Estimating steady state tracking variance...')
+            error_lists['az'] = []
+            error_lists['alt'] = []
+            position_start = mount.get_azalt()
+            time_start = time.time()
+            tracker.register_callback(tracker_callback)
+            tracker.run()
+            tracker.register_callback(None)
+            tracking_mean = {
+                'az': np.mean(error_lists['az']),
+                'alt': np.mean(error_lists['alt'])
+            }
+            tracking_sigma = {
+                'az': np.std(error_lists['az']),
+                'alt': np.std(error_lists['alt'])
+            }
+            position_stop = mount.get_azalt()
+            time_elapsed = time.time() - time_start
+            slew_rate_est = {
+                'az': (position_stop['az'] - position_start['az']) / time_elapsed,
+                'alt': (position_stop['alt'] - position_start['alt']) / time_elapsed
+            }
+            print('\taz tracking mean (arcseconds): ' + str(tracking_mean['az'] * 3600.0))
+            print('\talt tracking mean (arcseconds): ' + str(tracking_mean['alt'] * 3600.0))
+            print('\taz tracking sigma (arcseconds): ' + str(tracking_sigma['az'] * 3600.0))
+            print('\talt tracking sigma (arcseconds): ' + str(tracking_sigma['alt'] * 3600.0))
+            print('\taz slew rate (arcsec/s) ' + str(slew_rate_est['az'] * 3600.0) + ', ' + str(tracker.loop_filter['az'].int * 3600.0))
+            print('\talt slew rate (arcsec/s): ' + str(slew_rate_est['alt'] * 3600.0) + ', ' + str(tracker.loop_filter['alt'].int * 3600.0))
 
-            print('Object has been centered. Slewing in ' + axis + ' until movement detected...')
+            # estimate object's apparent motion with mount stationary in axis
+            # under test but continuing to slew in other axis
+            print('Estimating object apparent motion while tracking in single axis...')
+            mount.slew(axis, 0.0)
+            time.sleep(SLEW_STOP_SLEEP)
+            error_start = error_source.compute_error()
+            time_start = time.time()
+            tracker.register_callback(stop_at_half_frame_callback)
+            tracker.run(axes=[other_axis])
+            tracker.register_callback(None)
+            error_stop = error_source.compute_error()
+            time_elapsed = time.time() - time_start
+            apparent_motion = {
+                'az': (error_stop['az'] - error_start['az']) / time_elapsed,
+                'alt': (error_stop['alt'] - error_start['alt']) / time_elapsed
+            }
+            print('\taz apparent motion (arcsec/s): ' + str(apparent_motion['az'] * 3600.0))
+            print('\talt apparent motion (arcsec/s): ' + str(apparent_motion['alt'] * 3600.0))
 
-            # slew in axis under test until movement is detected
-            mount.slew(axis, FAST_SLEW_RATE)
-            while True:
-                error = error_source.compute_error()
-                if abs(error[axis] - init_error[axis]) > MOVEMENT_THRESHOLD_DEG:
-                    mount.slew(axis, 0.0)
-                    time.sleep(SLEW_STOP_SLEEP)
-                    break
+            # Compute ratio of sidreal rate estimated by the camera to the slew
+            # rate of the mount in the axis under consideration. Hopefully this
+            # is near 1.0, but in practice it may be a little bit off.
+            camera_to_mount_rate = np.absolute(apparent_motion['az'] + 1j*apparent_motion['alt']) / slew_rate_est[axis]
+            print('\tcamera to mount rate ratio: ' + str(camera_to_mount_rate))
 
-            # record position
-            init_error = error_source.compute_error()
-            init_position = mount.get_azalt()
+            # center the object in the FOV
+            print('Tracking object until converged...')
+            tracker.run()
 
-            print('Movement detected. Optical position error is ' + str(init_error[axis] * 3600.0) + ' arcseconds')
-            print('Slewing in other direction until movement detected...')
+            # slew opposite tracking direction until object is near edge of frame
+            print('Slewing in opposite of tracking direction in ' + str(axis) + '...')
+            error_start = error_source.compute_error()
+            position_start = mount.get_azalt()
+            time_start = time.time()
+            mount.slew(axis, -mount.last_slew_dir[axis] * SLOW_SLEW_RATE)
+            tracker.register_callback(stop_at_half_frame_callback)
+            tracker.run(axes=[other_axis])
+            tracker.register_callback(None)
+            error_stop = error_source.compute_error()
+            position_stop = mount.get_azalt()
+            time_elapsed = time.time() - time_start
 
-            # slew the other direction until movement is detected
-            mount.slew(axis, -SLOW_SLEW_RATE)
-            while True:
-                error = error_source.compute_error()
-                if abs(error[axis] - init_error[axis]) > MOVEMENT_THRESHOLD_DEG:
-                    position = mount.get_azalt()
-                    mount.slew(axis, 0.0)
-                    break
+            # calculations to estimate backlash deadband
+            uncorrected_motion = {
+                'az': error_stop['az'] - error_start['az'],
+                'alt': error_stop['alt'] - error_start['alt'],
+            }
+            predicted_sidereal_motion = {
+                'az': apparent_motion['az'] * time_elapsed,
+                'alt': apparent_motion['alt'] * time_elapsed,
+            }
+            corrected_motion = {
+                'az': uncorrected_motion['az'] - predicted_sidereal_motion['az'],
+                'alt': uncorrected_motion['alt'] - predicted_sidereal_motion['alt'],
+            }
+            mount_pos_change_predict = np.absolute(corrected_motion['az'] + 1j*corrected_motion['alt']) / camera_to_mount_rate
+            mount_pos_change = abs(position_stop[axis] - position_start[axis])
+            backlash_estimates[axis].append(abs(mount_pos_change - mount_pos_change_predict))
+            print('\telapsed time: ' + str(time_elapsed))
+            print('\tuncorrected motion: ' + str(uncorrected_motion))
+            print('\tpredicted sidereal motion: ' + str(predicted_sidereal_motion))
+            print('\tcorrected motion: ' + str(corrected_motion))
+            print('\tpredicted change in mount position: ' + str(mount_pos_change_predict))
+            print('\tmount reported change in position: ' + str(mount_pos_change))
 
-            backlash_estimates[axis].append(abs(errorsources.wrap_error(position[axis] - init_position[axis])))
-
-            print('Movement detected. Estimate of backlash is ' + str(backlash_estimates[axis][-1] * 3600.0) + ' arcseconds.')
+            print('Estimate of backlash deadband is ' + str(backlash_estimates[axis][-1] * 3600.0) + ' arcseconds.')
 
         print('Iterations for ' + axis + ' axis completed.')
         print('Mean backlash: ' + str(np.mean(backlash_estimates[axis]) * 3600.0) + ' arcseconds')
