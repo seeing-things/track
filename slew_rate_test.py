4990f8aca61dd7fe13e5fd8ffae544f0d036e606
Brett Gottula <bgottula@gmail.com>
2017-08-12 12:39:42 -0700

    #16: Adjustments to improve accuracy of slow slew rate estimates

--- slew_rate_test.py
+++ slew_rate_test.py
@@ -15,7 +15,8 @@ mount = mounts.NexStarMount(args.scope)
 
 try:
     SLEW_CHANGE_SLEEP = 3.0
-    TEST_DURATION = 5.0
+    TIME_LIMIT = 60.0
+    SLEW_LIMIT = 20.0
 
     # rates to test in arcseconds per second
     rates = [2**x for x in range(14)]
@@ -36,11 +37,15 @@ try:
             time.sleep(SLEW_CHANGE_SLEEP)
             position_start = mount.get_azalt()
             time_start = time.time()
-            time.sleep(TEST_DURATION)
-            position_stop = mount.get_azalt()
-            time_elapsed = time.time() - time_start
+            while True:
+                position = mount.get_azalt()
+                time_elapsed = time.time() - time_start
+                position_change = abs(errorsources.wrap_error(position[axis] - position_start[axis]))
+                if position_change > SLEW_LIMIT or time_elapsed > TIME_LIMIT:
+                    break
 
-            rate_est[axis].append(abs(errorsources.wrap_error(position_stop[axis] - position_start[axis])) / time_elapsed)
+            rate_est[axis].append(position_change / time_elapsed)
+            print('\tmeasured rate: ' + str(rate_est[axis][-1] * 3600.0))
 
         mount.slew(axis, 0.0)
 
