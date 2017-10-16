fe0d8bb70a57003b980cb8646df1bad7ccdde50c
Brett Gottula <bgottula@gmail.com>
2017-10-15 18:56:55 -0700

    Revert "Reimplement webcam code using multiprocessing library for concurrency"
    This reverts commit fd640183e963711d021522df1161d41f645777be.

--- webcam.py
+++ webcam.py
@@ -1,8 +1,5 @@
-import os
 import fcntl
 import select
-import datetime
-import multiprocessing
 import numpy as np
 import cv2
 import v4l2
@@ -64,26 +61,7 @@ class WebCam(object):
         self.camera.queue_all_buffers()
         self.camera.start()
 
-        self.proc_exit_r, self.proc_exit_w = os.pipe()
-
-        self.frames_out, self.frames_in = multiprocessing.Pipe(duplex=False)
-
-        self.proc = multiprocessing.Process(target=self.monitor, name='WebCam Process')
-        self.proc.start()
-
     def __del__(self):
-        if hasattr(self, 'proc') and self.proc.is_alive():
-            # first attempt to use the pipe/select/join method to cleanly end the monitor process
-            os.write(self.proc_exit_w, '!')
-            self.proc.join(1.0)
-            # failing that, kill it the brutal way
-            if self.proc.is_alive():
-                print('WebCam: monitor process didn\'t end; terminating it forcefully')
-                self.proc.terminate()
-        if hasattr(self, 'proc_exit_r'):
-            os.close(self.proc_exit_r)
-        if hasattr(self, 'proc_exit_w'):
-            os.close(self.proc_exit_w)
         if hasattr(self, 'camera'):
             self.camera.stop()
             self.camera.close()
@@ -101,15 +79,28 @@ class WebCam(object):
     # get the most recent frame from the webcam, waiting if necessary, and throwing away stale frames if any
     # (the frame is a numpy array in BGR format)
     def get_fresh_frame(self):
-        # block until there's at least one frame in the pipe
-        self.frames_in.poll(None)
+        self.block_until_frame_ready()
 
         frames = []
-        while self.frames_in.poll():
-            frames += [self.frames_in.read()]
+        while self.has_frames_available():
+            frames += [self.get_one_frame()]
 
-        # return only the most recently acquired frame
-        return frames[-1]
+        # decode the JPEG from the webcam into BGR for OpenCV's use
+        return cv2.imdecode(np.fromstring(frames[-1], dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    # get one frame from the webcam buffer; the frame is not guaranteed to be the most recent frame available!
+    # (the frame is a JPEG byte string)
+    def get_one_frame(self):
+        return self.camera.read_and_queue()
+
+    # block until the webcam has at least one frame ready
+    def block_until_frame_ready(self):
+        select.select((self.camera,), (), ())
+
+    # query whether the webcam has at least one frame ready for us to read (non-blocking)
+    def has_frames_available(self):
+        readable, writable, exceptional = select.select((self.camera,), (), (), 0.0)
+        return (len(readable) != 0)
 
     def is_ctrl_supported(self, id):
         query = v4l2.v4l2_queryctrl()
@@ -121,30 +112,3 @@ class WebCam(object):
         if ((query.flags & v4l2.V4L2_CTRL_FLAG_DISABLED) != 0):
             return False
         return True
-
-    # [WEBCAM PROCESS] webcam monitor loop
-    def monitor(self):
-        while True:
-            readable, writable, exceptional = select.select((self.camera, self.proc_exit_r), (), (), 0.5)
-
-            # the main thread is asking us to terminate
-            if self.proc_exit_r in readable:
-                return
-
-            # grab as many frames as are available to us right now
-            while self.has_frames_available():
-                now = datetime.datetime.utcnow() # TODO
-                frames_jpeg += [self.camera.read_and_queue()]
-
-            # decode the compressed JPEG image data into BGR format for OpenCV's use
-            for frame_jpeg in frames_jpeg:
-                frames_bgr += [cv2.imdecode(np.fromstring(frame_jpeg, dtype=np.uint8), cv2.IMREAD_COLOR)]
-
-            # send the decoded frames across the pipe to the main process
-            for frame_bgr in frames_bgr:
-                self.frames_in.send(frame_bgr)
-
-    # [WEBCAM PROCESS] does the webcam have at least one frame ready for us to read? (non-blocking)
-    def has_frames_available(self):
-        readable, writable, exceptional = select.select((self.camera,), (), (), 0.0)
-        return (self.camera in readable)
