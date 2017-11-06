cdf545cf9385a0490590cd0162141025a1301c09
Justin Gottula <justin@jgottula.com>
2017-11-05 21:21:18 -0800

    Use argparse formatter RawDescriptionHelpFormatter, maybe temporarily

--- track/config.py
+++ track/config.py
@@ -13,7 +13,8 @@ def ArgParser():
         ignore_unknown_config_file_keys =True,
         allow_abbrev                    =True,
         default_config_files            =DEFAULT_CONFIG_FILES,
-        formatter_class                 =configargparse.ArgumentDefaultsHelpFormatter,
+#        formatter_class                 =configargparse.ArgumentDefaultsHelpFormatter,
+        formatter_class                 =configargparse.RawDescriptionHelpFormatter,
         config_file_parser_class        =configargparse.DefaultConfigFileParser, # INI format
         args_for_setting_config_path    =['-c', '--cfg'],
         args_for_writing_out_config_file=['-w', '--cfg-write'],
