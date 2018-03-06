a3fb3b4da0a1b94949ec3862932447d97e940aea
Brett Gottula <bgottula@gmail.com>
2018-03-05 22:32:20 -0800

    Linting and bugfixes in optical_track

--- track/optical_track.py
+++ track/optical_track.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""Computer vision tracking of objects.
+
+This program uses a feedback control loop to cause a telescope mount to track an object based on
+computer vision detection of that object in a camera. The distance of the object from the center
+of the camera frame is used as an error term such that the control loop will attempt to center
+the object in the frame.
+
+An optional gamepad can be used to manually control the mount when no targets are in view. When
+a target is acquired by the camera gamepad control is inhibited.
+"""
+
+from __future__ import print_function
 import sys
 import track
 
@@ -58,16 +70,21 @@ def main():
     )
     args = parser.parse_args()
 
-    # this callback allows manual control of the slew rate via a gamepad when the
-    # control loop is not tracking any optical targets
     def gamepad_callback():
+        """Callback for gamepad control.
+
+        Allows manual control of the slew rate via a gamepad when the control loop is not tracking
+        any optical targets. Gamepad control is inhibited when a target is in view. This callback
+        is registered with the Tracker object which calls it on every control cycle.
+        """
+
         # don't try to fight the control loop
-        if tracker.error['az'] == None:
+        if not any(tracker.error.values()):
             try:
-                x, y = game_pad.get_proportional()
-                mount.slew('az', mount.max_slew_rate * x)
-                mount.slew('alt', mount.max_slew_rate * y)
-            except mount.AltitudeLimitException:
+                gamepad_x, gamepad_y = game_pad.get_proportional()
+                mount.slew(x_axis_name, mount.max_slew_rate * gamepad_x)
+                mount.slew(y_axis_name, mount.max_slew_rate * gamepad_y)
+            except mount.AxisLimitException:
                 pass
 
     # Create object with base type TelescopeMount
@@ -75,20 +92,31 @@ def main():
         mount = track.NexStarMount(args.mout_path, bypass_alt_limits=args.bypass_alt_limits)
         if args.bypass_alt_limits:
             print('Warning: Altitude limits disabled! Be careful!')
+        x_axis_name = 'az'
+        y_axis_name = 'alt'
     elif args.mount_type == 'gemini':
         mount = track.LosmandyGeminiMount(args.mount_path)
+        x_axis_name = 'ra'
+        y_axis_name = 'dec'
     else:
         print('mount-type not supported: ' + args.mount_type)
         sys.exit(1)
 
     # Create object with base type ErrorSource
-    error_source = track.OpticalErrorSource(args.camera, args.camera_res, args.camera_bufs, args.camera_exposure)
+    error_source = track.OpticalErrorSource(
+        cam_dev_path=args.camera,
+        arcsecs_per_pixel=args.camera_res,
+        cam_num_buffers=args.camera_bufs,
+        cam_ctlval_exposure=args.camera_exposure,
+        x_axis_name=x_axis_name,
+        y_axis_name=y_axis_name
+    )
 
     tracker = track.Tracker(
-        mount = mount,
-        error_source = error_source,
-        loop_bandwidth = args.loop_bw,
-        damping_factor = args.loop_damping
+        mount=mount,
+        error_source=error_source,
+        loop_bandwidth=args.loop_bw,
+        damping_factor=args.loop_damping
     )
 
     try:
@@ -102,7 +130,6 @@ def main():
         tracker.run()
     except KeyboardInterrupt:
         print('Goodbye!')
-        pass
 
 if __name__ == "__main__":
     main()
