d2d5f817a13c391ae14e06d7f346ac32d7bc227b
Brett Gottula <bgottula@gmail.com>
2020-04-10 01:12:52 -0700

    #28: Adds AcceleratingMountAxisTarget for testing control system

--- track/targets.py
+++ track/targets.py
@@ -1,10 +1,14 @@
 """targets for use in telescope tracking control loop"""
 
+from typing import Tuple
+import time
 from abc import ABC, abstractmethod
-from astropy.coordinates import EarthLocation, SkyCoord, AltAz
+from astropy.coordinates import EarthLocation, SkyCoord, AltAz, Longitude
 from astropy.time import Time
 from astropy import units as u
 import ephem
+from track.model import MountModel
+from track.mounts import MountEncoderPositions
 
 
 class Target(ABC):
@@ -39,6 +43,51 @@ class FixedTopocentricTarget(Target):
     def get_position(self, t: Time = None) -> SkyCoord:
         """Since the topocentric position is fixed the t argument is ignored"""
         return self.coord
+
+
+class AcceleratingMountAxisTarget(Target):
+    """A target that accelerates at a constant rate in one or both mount axes.
+
+    This target is intented for testing the control system's ability to track an accelerating
+    target with reasonably small steady-state error.
+    """
+
+    def __init__(
+            self,
+            mount_model: MountModel,
+            initial_encoder_positions: MountEncoderPositions,
+            axis_accelerations: Tuple[float, float],
+        ):
+        """Construct an AcceleratingMountAxisTarget.
+
+        Initial velocity of the target is zero in both axes. Acceleration begins the moment this
+        constructor is called and continues forever without limit as currently implemented.
+
+        Args:
+            mount_model: An instance of the mount model. This is required because the acceleration
+                is meant to be held constant in the mount axes, yet the API of the Target class
+                requires a topocentric coordinate be returned by the `get_position()` method.
+            initial_encoder_positions: The starting positions of the mount encoders. Note that if
+                axis 1 starts out pointed at the pole and has acceleration 0 axis 0 may not behave
+                as expected since the pole is a singularity. A work-round is to set the initial
+                encoder position for axis 1 to a small offset from the pole, or to use a non-zero
+                acceleration for axis 1.
+            axis_accelerations: A tuple of two floats giving the accelerations in axis 0 and axis
+                1, respectively, in degrees per second squared (negative okay).
+        """
+        self.mount_model = mount_model
+        self.accel = axis_accelerations
+        self.time_start = Time.now()
+        self.initial_positions = initial_encoder_positions
+
+    def get_position(self, t: Time):
+        """Gets the position of the simulated target for a specific time."""
+        time_elapsed = (t - self.time_start).sec
+        target_encoder_positions = MountEncoderPositions(
+            Longitude((self.initial_positions[0].deg + self.accel[0] * time_elapsed**2) * u.deg),
+            Longitude((self.initial_positions[1].deg + self.accel[1] * time_elapsed**2) * u.deg),
+        )
+        return self.mount_model.encoders_to_topocentric(target_encoder_positions)
 
 
 class PyEphemTarget(Target):
