e0de76db02380312de04c71579c56f41b68b45c7
Brett Gottula <bgottula@gmail.com>
2021-03-01 04:04:19 -0800

    Upgrade to InfluxDB 2.0 client

--- track/telem.py
+++ track/telem.py
@@ -1,13 +1,19 @@
+"""telemetry logging to a time series database"""
+
 from abc import ABC, abstractmethod
 import os
+from datetime import datetime
+import pathlib
 import time
 import threading
+import toml
 import traceback
-import influxdb
-from influxdb.exceptions import InfluxDBClientError, InfluxDBServerError
-import datetime
 from configargparse import Namespace
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import WriteOptions
+from influxdb_client.client.exceptions import InfluxDBError
 from track.config import ArgParser
+
 
 class TelemSource(ABC):
     """Abstract base class for a producer of telemetry.
@@ -37,18 +43,19 @@ class TelemLogger:
     Telemetry is sampled in a thread that executes at a regular rate.
 
     Attributes:
-        db: InfluxDBClient object.
-        thread: Thread for sampling telemetry from sources.
+        bucket: Name of bucket in the database to write to.
+        influxdb_client: InfluxDBClient object.
         period: Telemetry period in seconds.
-        sources: Dict of TelemSource objects to be polled for telemetry.
         running: Boolean indicating whether the logger is running or not.
+        sources: Dict of TelemSource objects to be polled for telemetry.
+        thread: Thread for sampling telemetry from sources.
+        write_api: InfluxDBClient write API.
     """
 
     def __init__(
         self,
-        host: str = 'localhost',
-        port: int = 8086,
-        dbname: str = 'telem',
+        influx_config_filename: str,
+        bucket: str = 'telem',
         period: float = 1.0,
         sources: dict = {}
     ):
@@ -58,18 +65,40 @@ class TelemLogger:
         Telemetry sampling will not occur until the start() method is called.
 
         Args:
-            host: Hostname of machine running the InfluxDB database.
-            port: Port number the InfluxDB database listens on.
-            dbname: Name of database.
+            influx_config_filename: Filename of the InfluxDB CLI configuration file, typically
+                $HOME/.influxdbv2/configs which is created by running `influx setup`.
+            bucket: Name of bucket in database to write to.
             period: Telemetry will be sampled at this interval in seconds.
             sources: Dict of TelemSource objects to be polled for telemetry. Keys will be used as
                 the measurement names in the database. Values should be objects of type
                 TelemSource.
         """
-        self.db = influxdb.InfluxDBClient(host=host, port=port, database=dbname)
-        self.thread = threading.Thread(target=self._worker_thread, name='TelemLogger: worker thread')
+        self.bucket = bucket
         self.period = period
         self.sources = sources
+
+        # Can't use `InfluxDBClient.from_config_file()` here because `influx setup` generates a
+        # TOML formatted config file, but the Python client expects a .ini file. Because of course.
+        config_file_dict = toml.load(influx_config_filename)
+        self.influxdb_client = InfluxDBClient(
+            url=config_file_dict['influx2']['url'],
+            token=config_file_dict['influx2']['token'],
+            org=config_file_dict['influx2']['org'],
+        )
+
+        self.write_api = self.influxdb_client.write_api(
+            write_options=WriteOptions(
+                batch_size=500,  # number of data points per batch
+                flush_interval=1000,  # milliseconds before batch is written
+                jitter_interval=0,
+                retry_interval=1000,  # milliseconds before retry after failed write
+                max_retries=5,
+                max_retry_delay=10_000,  # give up after this many milliseconds
+                exponential_base=2
+            )
+        )
+
+        self.thread = threading.Thread(target=self._worker_thread, name='TelemLogger: worker thread')
         self.running = False
 
     def start(self) -> None:
@@ -97,15 +126,16 @@ class TelemLogger:
         """
         if not channels:
             return
-        timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
-        json_body = [
-            {
-                "measurement": name,
-                "time": timestamp,
-                "fields": channels
-            }
-        ]
-        self.db.write_points(json_body)
+
+        point = Point.from_dict(
+            dictionary={
+                'measurement': name,
+                'fields': channels,
+                'time': datetime.utcnow(),
+            },
+            write_precision=WritePrecision.NS,
+        )
+        self.write_api.write(bucket=self.bucket, record=point)
 
     def _worker_thread(self) -> None:
         """Gathers telemetry and posts to database once per sample period."""
@@ -120,7 +150,7 @@ class TelemLogger:
             try:
                 for name, source in self.sources.items():
                     self._post_point(name, source.get_telem_channels())
-            except (InfluxDBClientError, InfluxDBServerError) as e:
+            except InfluxDBError as e:
                 print('Failed to post telemetry to database: ' + str(e))
                 traceback.print_exc()
             elapsed_time = time.time() - start_time
@@ -145,15 +175,9 @@ def add_program_arguments(parser: ArgParser) -> None:
         action='store_true'
     )
     telem_group.add_argument(
-        '--telem-db-host',
-        help='hostname of InfluxDB database server',
-        default='localhost'
-    )
-    telem_group.add_argument(
-        '--telem-db-port',
-        help='port number of InfluxDB database server',
-        default=8086,
-        type=int
+        '--telem-influxdb-configfile',
+        help='filename of InfluxDB CLI config file',
+        default=str(pathlib.Path.home().joinpath('.influxdbv2/configs'))
     )
     telem_group.add_argument(
         '--telem-period',
@@ -171,8 +195,7 @@ def make_telem_logger_from_args(args: Namespace, sources: dict = {}) -> TelemLog
         sources: Dict of TelemSources to be passed to TelemLogger constructor.
     """
     return TelemLogger(
-        host=args.telem_db_host,
-        port=args.telem_db_port,
+        influx_config_filename=args.telem_influxdb_configfile,
         period=args.telem_period,
         sources=sources,
     )
