0e1a91e9bc462acde2c9b9d837b2dcc97083a178
Brett Gottula <bgottula@gmail.com>
2021-06-03 23:13:38 -0700

    Fix bug in IERS download during installation

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 # To use a consistent encoding
+import site
+from importlib import reload
 from codecs import open
 from os import path
 from setuptools import setup, find_packages
@@ -30,6 +32,7 @@ def download_iers():
     https://docs.astropy.org/en/stable/api/astropy.utils.iers.IERS_Auto.html
     https://docs.astropy.org/en/stable/api/astropy.utils.iers.LeapSeconds.html
     """
+    reload(site)
     from astropy.utils import iers
     iers.IERS_Auto().open()
     iers.LeapSeconds.auto_open()
